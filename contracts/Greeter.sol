3f0f687202675cef62fff994c72f888df60ed516
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-04-25 12:53:39 +0900

    4.5

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,20 +1,9 @@
 pragma solidity >=0.4.22 <0.9.0;
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract Greeter {
+contract Greeter is Ownable{
     address private _owner;
     string private _greeting = "Hello, World!";
-
-    constructor() public {
-        _owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(
-            msg.sender == _owner,
-            "Ownable: caller is not the owner"
-        );
-        _;
-    }
 
     function greet() external view returns (string memory){
         return _greeting;
@@ -22,7 +11,4 @@ contract Greeter {
     function setGreeting (string calldata greeting) external onlyOwner{
         _greeting = greeting;
     }
-    function owner() public view returns (address){
-        return _owner;
-    }    
 }
